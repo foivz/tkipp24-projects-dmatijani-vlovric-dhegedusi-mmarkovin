bddd39f79ee8f3b4193887d38c36426747fc5c32
David Matijanić <dmatijani21@foi.hr>
2024-04-11 19:06:33 +0200

    #10 Napisan Gherkin i step definitions za F01_S05

---
+++ AcceptanceTests/AcceptanceTests/Features/Functionality_01_s05.feature
@@ -0,0 +1,17 @@
+﻿Feature: Admin employee view
+
+As an administrator, I want to be able to see all employees for a chosen library in the system
+and also know if there are no employees for the chosen library
+
+Background:
+	Given the user is logged in as an administrator
+	And the user is on the All employees screen
+
+Scenario: No employees
+	When the administrator chooses a library with no employees
+	Then the user should be notified that there are no employees for the chosen library
+
+Scenario: Showing employees
+	When the administrator chooses a library with atleast one employee
+	Then the user should be shown the employees for that library
+	
---
+++ AcceptanceTests/AcceptanceTests/StepDefinitions/F01_S05_AdminLibraryViewStepDefinitions.cs
@@ -0,0 +1,65 @@
+using AcceptanceTests.Support;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Linq;
+using TechTalk.SpecFlow;
+
+namespace AcceptanceTests.StepDefinitions
+{
+    [Binding]
+    public class F01_S05_AdminLibraryViewStepDefinitions
+    {
+        [Given(@"the user is on the All employees screen")]
+        public void GivenTheUserIsOnTheAllEmployeesScreen()
+        {
+            var driver = GuiDriver.GetDriver();
+            driver.SwitchTo().Window(driver.WindowHandles.First());
+
+            var btnAllLibraries = driver.FindElementByAccessibilityId("btnAllEmployees");
+            btnAllLibraries.Click();
+
+            var dgAllEmployees = driver.FindElementByAccessibilityId("dgAllEmployees");
+            Assert.IsNotNull(dgAllEmployees);
+        }
+
+        [When(@"the administrator chooses a library with no employees")]
+        public void WhenTheAdministratorChoosesALibraryWithNoEmployees()
+        {
+            var driver = GuiDriver.GetDriver();
+            var cboLibrary = driver.FindElementByAccessibilityId("cboLibrary");
+            cboLibrary.Click();
+
+            var libraryWithNoEmployees = driver.FindElementByName("12343 - Knjnaziv");
+            libraryWithNoEmployees.Click();
+        }
+
+        [Then(@"the user should be notified that there are no employees for the chosen library")]
+        public void ThenTheUserShouldBeNotifiedThatThereAreNoEmployeesForTheChosenLibrary()
+        {
+            var driver = GuiDriver.GetDriver();
+            var noEmployeesMessage = driver.FindElementByAccessibilityId("noEmployeesMessage");
+            Assert.IsNotNull(noEmployeesMessage);
+            GuiDriver.Dispose();
+        }
+
+        [When(@"the administrator chooses a library with atleast one employee")]
+        public void WhenTheAdministratorChoosesALibraryWithAtleastOneEmployee()
+        {
+            var driver = GuiDriver.GetDriver();
+            var cboLibrary = driver.FindElementByAccessibilityId("cboLibrary");
+            cboLibrary.Click();
+
+            var libraryWithNoEmployees = driver.FindElementByName("123 - Gradska knjižnica i citaonica Varaždin");
+            libraryWithNoEmployees.Click();
+        }
+
+        [Then(@"the user should be shown the employees for that library")]
+        public void ThenTheUserShouldBeShownTheEmployeesForThatLibrary()
+        {
+            var driver = GuiDriver.GetDriver();
+            var firstEmployee = driver.FindElementByName("pcindric89");
+            Assert.IsNotNull(firstEmployee);
+            GuiDriver.Dispose();
+        }
+    }
+}
