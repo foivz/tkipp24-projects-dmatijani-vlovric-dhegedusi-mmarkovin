5ad8333e2fc3d1a8a7e29664ee786d2f49b1e5a3
vlovric21 <vlovric21@student.foi.hr>
2024-04-13 18:27:29 +0200

    F03_S02 automatizirano napravljeno
    Pada jedan koji i treba past

--- AcceptanceTests/AcceptanceTests/Features/Functionality_03_s02.feature
+++ AcceptanceTests/AcceptanceTests/Features/Functionality_03_s02.feature
@@ -7,39 +7,39 @@ Background:
 	And the user is on the Add new book copies screen
 
 @tag1
-Scenario: Empty number of copies field
+Scenario: F03_S02_C01 Empty number of copies field
 	When the employee chooses a book from the list
 	And the employee leaves the number of copies field empty
 	And the employee clicks the Insert button
-	Then the employee should remain on the same screen
-	And the employee should see a warning message that the number of copies field cannot be empty
+	Then the employee should remain on the Add new book copies screen
+	And the employee should see a warning message that the number of new copies field cannot be empty
 
-Scenario: Non numerical entry for number of copies field
+Scenario: F03_S02_C02 Non numerical entry for number of copies field
 	When the employee chooses a book from the list
 	And the employee enters a non numerical or negative value into the number of copies field
 	And the employee clicks the Insert button
-	Then the employee should remain on the same screen
+	Then the employee should remain on the Add new book copies screen
 	And the employee should see a warning message that the number of copies field has to contain a valid numerical value
 
-Scenario: Book not chosen
+Scenario: F03_S02_C03 Book not chosen
 	When the employee does not choose a book from the list
 	And the employee enters a valid number of copies
 	And the employee clicks the Insert button
-	Then the employee should remain on the same screen
+	Then the employee should remain on the Add new book copies screen
 	And the employee should see a warning message that a book must be chosen
 
-Scenario: Book copies succesfully added
+Scenario: F03_S02_C04 Book copies succesfully added
 	When the employee chooses a book from the list
 	And the employee enters a valid number of copies
 	And the employee clicks the Insert button
-	Then the employee should remain on the same screen
+	Then the employee should remain on the Add new book copies screen
 	And the employee should see a message that the copies have been succesfully added
 	And the employee should see a refreshed list
 
-Scenario: Back button from Add new book copies leads to Action choice screen
+Scenario: F03_S02_C05 Back button from Add new book copies leads to Action choice screen
 	When the employee clicks on the Back button
 	Then the employee should be redirected to the Action choice screen
 
-Scenario: Book search works
+Scenario: F03_S02_C06 Book search works
 	When the employee enters an existing book name into the book search field
 	Then the empoloyee sees the desired book in the list
--- AcceptanceTests/AcceptanceTests/StepDefinitions/F03_S02_NewBookCopiesStepDefinitions.cs
+++ AcceptanceTests/AcceptanceTests/StepDefinitions/F03_S02_NewBookCopiesStepDefinitions.cs
@@ -1,4 +1,7 @@
+using AcceptanceTests.Support;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.Linq;
 using TechTalk.SpecFlow;
 
 namespace AcceptanceTests.StepDefinitions
@@ -9,61 +12,109 @@ namespace AcceptanceTests.StepDefinitions
         [Given(@"the user is on the Add new book copies screen")]
         public void GivenTheUserIsOnTheAddNewBookCopiesScreen()
         {
-            throw new PendingStepException();
+            var driver = GuiDriver.GetDriver();
+            driver.SwitchTo().Window(driver.WindowHandles.First());
+
+            var btnCatalogue = driver.FindElementByAccessibilityId("btnBookCatalog");
+            btnCatalogue.Click();
+
+            var btnNewBook = driver.FindElementByAccessibilityId("btnNewCopies");
+            btnNewBook.Click();
         }
+
+        [Then(@"the employee should remain on the Add new book copies screen")]
+        public void ThenTheEmployeeShouldRemainOnTheAddNewBookCopiesScreen()
+        {
+            var driver = GuiDriver.GetDriver();
+
+            bool screen = driver.FindElementByName("Dodajte nove primjerke knjige") != null;
+            Assert.IsTrue(screen);
+        }
+
 
         [When(@"the employee chooses a book from the list")]
         public void WhenTheEmployeeChoosesABookFromTheList()
         {
-            throw new PendingStepException();
+            var driver = GuiDriver.GetDriver();
+
+            var btnBook = driver.FindElementByName("Ime 1");
+            btnBook.Click();
         }
+
+        [Then(@"the employee should see a warning message that the number of new copies field cannot be empty")]
+        public void ThenTheEmployeeShouldSeeAWarningMessageThatTheNumberOfNewCopiesFieldCannotBeEmpty()
+        {
+            var driver = GuiDriver.GetDriver();
+
+            bool screen = driver.FindElementByName("Broj novih primjeraka mora sadržavati samo brojeve!") != null;
+            Assert.IsTrue(screen);
+        }
+
 
         [Then(@"the employee should see a warning message that the number of copies field has to contain a valid numerical value")]
         public void ThenTheEmployeeShouldSeeAWarningMessageThatTheNumberOfCopiesFieldHasToContainAValidNumericalValue()
         {
-            throw new PendingStepException();
+            var driver = GuiDriver.GetDriver();
+
+            bool screen = driver.FindElementByName("Broj novih primjeraka mora sadržavati samo brojeve!") != null;
+            Assert.IsTrue(screen);
         }
 
         [When(@"the employee does not choose a book from the list")]
         public void WhenTheEmployeeDoesNotChooseABookFromTheList()
         {
-            throw new PendingStepException();
+            //nothing
         }
 
         [When(@"the employee enters a valid number of copies")]
         public void WhenTheEmployeeEntersAValidNumberOfCopies()
         {
-            throw new PendingStepException();
+            var driver = GuiDriver.GetDriver();
+
+            var txtCopies = driver.FindElementByAccessibilityId("txtNumberCopies");
+            txtCopies.SendKeys("1");
         }
 
         [Then(@"the employee should see a warning message that a book must be chosen")]
         public void ThenTheEmployeeShouldSeeAWarningMessageThatABookMustBeChosen()
         {
-            throw new PendingStepException();
+            var driver = GuiDriver.GetDriver();
+
+            bool screen = driver.FindElementByName("Morate odabrati knjigu!") != null;
+            Assert.IsTrue(screen);
         }
 
         [Then(@"the employee should see a message that the copies have been succesfully added")]
         public void ThenTheEmployeeShouldSeeAMessageThatTheCopiesHaveBeenSuccesfullyAdded()
         {
-            throw new PendingStepException();
+            var driver = GuiDriver.GetDriver();
+
+            bool screen = driver.FindElementByName("Uspješno!") != null;
+            Assert.IsTrue(screen);
         }
 
         [Then(@"the employee should see a refreshed list")]
         public void ThenTheEmployeeShouldSeeARefreshedList()
         {
-            throw new PendingStepException();
+            //nothing
         }
 
         [When(@"the employee enters an existing book name into the book search field")]
         public void WhenTheEmployeeEntersAnExistingBookNameIntoTheBookSearchField()
         {
-            throw new PendingStepException();
+            var driver = GuiDriver.GetDriver();
+
+            var txtCopies = driver.FindElementByAccessibilityId("txtBookName");
+            txtCopies.SendKeys("Labirint");
         }
 
         [Then(@"the empoloyee sees the desired book in the list")]
         public void ThenTheEmpoloyeeSeesTheDesiredBookInTheList()
         {
-            throw new PendingStepException();
+            var driver = GuiDriver.GetDriver();
+
+            bool screen = driver.FindElementByName("Labirint Iluzija") != null;
+            Assert.IsTrue(screen);
         }
     }
 }
