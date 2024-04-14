7b2a3013ab81ccb724256453a04f473296440e1e
Magdalena Markovinović <mmarkovin21@foi.hr>
2024-04-14 15:01:10 +0200

    #) Popravljene neke greške.
    I dalje su ljubičasta slova na gherkin sintaksi.

--- AcceptanceTests/AcceptanceTests/Features/Functionality_06_s01.feature
+++ AcceptanceTests/AcceptanceTests/Features/Functionality_06_s01.feature
@@ -11,14 +11,22 @@ Background:
 Scenario Outline: Filtering members by name and surename
 	When Employee enters name <name> and surename <surename>
 	And clicks the Filter button
-	Then application shows <name> <surename>
+	Then application shows <name> and <surename>
 
 	Examples: 
-		| username   | surename |
+		| name | surename |
 		| Sebastijan |          | 
 		|            | Kralj    |
 		| Sebastijan | Kralj    |
-		| Petra      | Perković |
+
+Scenario: Filtering members that do not exist
+	When Employee enters name <name> and surename <surename>
+	And clicks the Filter button
+	Then application shows empty table
+
+	Examples: 
+		| name   | surename |
+		| Petra | Perković |
 
 Scenario: Clearing the filter
 	When Employee enters name <name> and surename <surename>
@@ -26,5 +34,5 @@ Scenario: Clearing the filter
 	Then application shows all members
 
 	Examples: 
-		| username   | surename |
+		| name  | surename |
 		| Sebastijan | Kralj    |
--- AcceptanceTests/AcceptanceTests/StepDefinitions/F06_S01_FilteringMembers.cs
+++ AcceptanceTests/AcceptanceTests/StepDefinitions/F06_S01_FilteringMembers.cs
@@ -1,7 +1,10 @@
 using AcceptanceTests.Support;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
 using System;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Xml.Linq;
 using TechTalk.SpecFlow;
 
 namespace AcceptanceTests.StepDefinitions
@@ -17,8 +20,8 @@ namespace AcceptanceTests.StepDefinitions
             var txtUsername = driver.FindElementByAccessibilityId("txtUsername");
             var txtPassword = driver.FindElementByAccessibilityId("txtPassword");
 
-            txtUsername.SendKeys("pcindric89");
-            txtPassword.SendKeys("cindricka123");
+            txtUsername.SendKeys("hmihovic");
+            txtPassword.SendKeys("volimcitati");
             var btnLogin = driver.FindElementByAccessibilityId("btnLogin");
             btnLogin.Click();
 
@@ -29,8 +32,12 @@ namespace AcceptanceTests.StepDefinitions
         {
             var driver = GuiDriver.GetDriver();
             driver.SwitchTo().Window(driver.WindowHandles.First());
+            driver.Manage().Window.Maximize();
             var btnMembership = driver.FindElementByAccessibilityId("btnMembership");
             btnMembership.Click();
+
+            var membersPanel = driver.FindElementByName("Èlanovi knjižnice");
+            Assert.IsNotNull(membersPanel);
         }
 
         [When(@"Employee enters name (.*) and surename (.*)")]
@@ -38,33 +45,63 @@ namespace AcceptanceTests.StepDefinitions
         {
             var driver = GuiDriver.GetDriver();
             var txtFilter = driver.FindElementByAccessibilityId("txtFilter");
-            string[] words = txtFilter.Text.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
 
-            Assert.IsTrue(words[0] == name && words[1] == surename);
+            txtFilter.SendKeys(name + " " + surename);
         }
 
         [When(@"clicks the Filter button")]
         public void WhenClicksTheFilterButton()
         {
-            Assert.IsTrue(true);
+            var driver = GuiDriver.GetDriver();
+            var btnFilter = driver.FindElementByAccessibilityId("btnFilter");
+            btnFilter.Click();
         }
 
-        [Then(@"application shows <name> ")]
-        public void ThenApplicationShowsName()
+        [Then(@"application shows empty table")]
+        public void ThenApplicationShowsEmptyTable()
         {
-            Assert.IsTrue(true);
+            var driver = GuiDriver.GetDriver();
+            var dgvName = driver.FindElementsByName("Petra");
+            bool nameExists = dgvName.Count != 0;
+            Assert.IsFalse(nameExists);
+            var dgvSurename = driver.FindElementsByName("Perkoviæ");
+            bool surnameExists = dgvSurename.Count != 0;
+            Assert.IsFalse(surnameExists);
+
+            GuiDriver.Dispose();
+
+        }
+
+        [Then(@"application shows (.*) and (.*) ")]
+        public void ThenApplicationShowsNameAndSurename(string name, string surename)
+        {
+            var driver = GuiDriver.GetDriver();
+            var dgvName = driver.FindElementByName(name);
+            Assert.IsNotNull(dgvName);
+            var dgvSurename = driver.FindElementByName(surename);
+            Assert.IsNotNull(dgvSurename);
+
+            GuiDriver.Dispose();
         }
 
         [When(@"Employee clicks the clear button")]
         public void WhenEmployeeClicksTheClearButton()
         {
-            Assert.IsTrue(true);
+            var driver = GuiDriver.GetDriver();
+            var btnClearFilter = driver.FindElementByAccessibilityId("btnClearFilter");
+            btnClearFilter.Click();
         }
 
         [Then(@"application shows all members")]
         public void ThenApplicationShowsAllMembers()
         {
-            Assert.IsTrue(true);
+            var driver = GuiDriver.GetDriver();
+            var dgvMembers = driver.FindElementByAccessibilityId("dgvMembers");
+            var rows = dgvMembers.FindElementsByName("456");
+            bool rowsExist = rows.Count() > 3;
+            Assert.IsNotNull(rowsExist);
+
+            GuiDriver.Dispose();
         }
     }
 }
