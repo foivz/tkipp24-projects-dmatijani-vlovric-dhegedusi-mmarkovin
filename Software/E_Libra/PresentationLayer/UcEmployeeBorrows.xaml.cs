08112cc52bd27922f1de80076a2697cee179996c
David Matijanić <dmatijani21@foi.hr>
2024-06-05 23:45:01 +0200

    #21 Sada se prikazuje loader kod učitavanja posudbi i ispis ako ih nema

--- Software/E_Libra/PresentationLayer/UcEmployeeBorrows.xaml.cs
+++ Software/E_Libra/PresentationLayer/UcEmployeeBorrows.xaml.cs
@@ -26,24 +26,40 @@ namespace PresentationLayer {
             InitializeComponent();
 
             this.mainWindow = _mainWindow;
-
-            LoadAllBorrows();
         }
 
-        public async void LoadAllBorrows() {
+        public async Task LoadAllBorrows() {
             await GetAllBorrowsForLibrary(LoggedUser.LibraryId);
             await GetBorrowsForEachStatus(LoggedUser.LibraryId);
         }
 
         private async Task GetAllBorrowsForLibrary(int libraryId) {
-            dgAllBorrows.ItemsSource = await borrowService.GetAllBorrowsForLibraryAsync(libraryId);
+            imgLoaderAllBorrows.Visibility = Visibility.Visible;
+            txtNoAllBorrows.Visibility = Visibility.Hidden;
+            var borrows = await borrowService.GetAllBorrowsForLibraryAsync(libraryId);
+            dgAllBorrows.ItemsSource = borrows;
+            if (borrows.Count == 0) {
+                txtNoAllBorrows.Visibility = Visibility.Visible;
+            }
+            imgLoaderAllBorrows.Visibility = Visibility.Hidden;
         }
 
         private async Task GetBorrowsForEachStatus(int libraryId) {
-            dgPendingBorrows.ItemsSource = await borrowService.GetBorrowsForLibraryByStatusAsync(libraryId, BorrowStatus.Waiting);
-            dgCurrentBorrows.ItemsSource = await borrowService.GetBorrowsForLibraryByStatusAsync(libraryId, BorrowStatus.Borrowed);
-            dgLateBorrows.ItemsSource = await borrowService.GetBorrowsForLibraryByStatusAsync(libraryId, BorrowStatus.Late);
-            dgDoneBorrows.ItemsSource = await  borrowService.GetBorrowsForLibraryByStatusAsync(libraryId, BorrowStatus.Returned);
+            await GetBorrowsForOneStatus(libraryId, BorrowStatus.Waiting, txtNoPendingBorrows, imgLoaderPendingBorrows, dgPendingBorrows);
+            await GetBorrowsForOneStatus(libraryId, BorrowStatus.Borrowed, txtNoCurrentBorrows, imgLoaderCurrentBorrows, dgCurrentBorrows);
+            await GetBorrowsForOneStatus(libraryId, BorrowStatus.Late, txtNoLateBorrows, imgLoaderLateBorrows, dgLateBorrows);
+            await GetBorrowsForOneStatus(libraryId, BorrowStatus.Returned, txtNoDoneBorrows, imgLoaderDoneBorrows, dgDoneBorrows);
+        }
+
+        private async Task GetBorrowsForOneStatus(int libraryId, BorrowStatus status, Border msgNone, Image loader, DataGrid grid) {
+            loader.Visibility = Visibility.Visible;
+            msgNone.Visibility = Visibility.Hidden;
+            var borrows = await borrowService.GetBorrowsForLibraryByStatusAsync(libraryId, status);
+            grid.ItemsSource = borrows;
+            if (borrows.Count == 0) {
+                msgNone.Visibility = Visibility.Visible;
+            }
+            loader.Visibility = Visibility.Hidden;
         }
 
         private void btnReturnBook_Click(object sender, RoutedEventArgs e) {
@@ -112,6 +128,22 @@ namespace PresentationLayer {
             } else {
                 mainWindow.contentPanel.Content = new UcBorrowNewBook(mainWindow, this, memberBarcode, bookBarcode);
             }
+        }
+
+        private async void UserControl_Loaded(object sender, RoutedEventArgs e) {
+            SetDefaultLoaderAndMessageVisibility();
+            await LoadAllBorrows();
+        }
+
+        private void SetDefaultLoaderAndMessageVisibility() {
+            txtNoAllBorrows.Visibility = Visibility.Hidden;
+            txtNoPendingBorrows.Visibility = Visibility.Hidden;
+            txtNoCurrentBorrows.Visibility = Visibility.Hidden;
+            txtNoDoneBorrows.Visibility = Visibility.Hidden;
+            imgLoaderAllBorrows.Visibility = Visibility.Hidden;
+            imgLoaderPendingBorrows.Visibility = Visibility.Hidden;
+            imgLoaderCurrentBorrows.Visibility = Visibility.Hidden;
+            txtNoDoneBorrows.Visibility = Visibility.Hidden;
         }
     }
 }
