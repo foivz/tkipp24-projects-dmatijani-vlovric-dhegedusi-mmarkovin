f47c208fdaecdc6fc99c29c2a64a4b79e138c388
vlovric21 <vlovric21@student.foi.hr>
2024-06-09 19:02:22 +0200

    Preimenovana testna klasa

---
+++ Software/E_Libra/UnitTesting/ArchiveServices_unitTest.cs
@@ -0,0 +1,25 @@
+﻿using DataAccessLayer.Interfaces;
+using DataAccessLayer.Repositories;
+using FakeItEasy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace UnitTesting
+{
+    public class ArchiveServices_unitTest
+    {
+        [Fact]
+        public void GetArchive_GivenFunctionIsCalled_ReturnsArchiveList()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+        }
+    }
+}
