0391a86c7040529293a86a6af9ba867d0844da28
vlovric21 <vlovric21@student.foi.hr>
2024-06-09 15:25:13 +0200

    Kreirana klasa za F03

---
+++ Software/E_Libra/UnitTesting/F03_unit.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace UnitTesting
+{
+    public class F03_unit
+    {
+        [Fact]
+        public void Test_True()
+        {
+            Assert.True(true);
+        }
+    }
+}
