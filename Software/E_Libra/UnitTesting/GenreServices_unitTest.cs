98eb71d668c883eecfa5e12c0d82a804fa803983
vlovric21 <vlovric21@student.foi.hr>
2024-06-10 15:13:20 +0200

    Napisan GenreServices

---
+++ Software/E_Libra/UnitTesting/GenreServices_unitTest.cs
@@ -0,0 +1,56 @@
+﻿using BussinessLogicLayer.services;
+using DataAccessLayer.Interfaces;
+using EntitiesLayer;
+using FakeItEasy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace UnitTesting
+{
+    public class GenreServices_unitTest
+    {
+
+        [Fact]
+        public void GetGenres_GivenFunctionIsCalled_ReturnsListOfGenres()
+        {
+            //Arrange
+            var genreList = new List<Genre>
+            {
+                new Genre { name = "Genre1" },
+                new Genre { name = "Genre2" }
+            }.AsQueryable();
+
+            var repo = A.Fake<IGenreRepository>();
+
+            var service = new GenreServices(repo);
+            A.CallTo(() => repo.GetAll()).Returns(genreList);
+
+            //Act
+            var result = service.GetGenres();
+
+            //Assert
+            Assert.Equal(genreList, result);
+        }
+
+        [Fact]
+        public void Add_GivenFunctionIsCalled_ReturnsTrue()
+        {
+            //Arrange
+            var repo = A.Fake<IGenreRepository>();
+            var genre = new Genre { name = "Genre1" };
+            var service = new GenreServices(repo);
+
+            A.CallTo(() => repo.Add(genre, true)).Returns(1);
+
+            //Act
+            var result = service.Add(genre);
+
+            //Assert
+            Assert.True(result);
+        }
+    }
+}
