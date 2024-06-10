3913d49206b315a657c2bc626da805295b99d6c9
vlovric21 <vlovric21@student.foi.hr>
2024-06-10 15:51:15 +0200

    Postavljena ReservationService testna klasa

---
+++ Software/E_Libra/UnitTesting/ReservationService_unitTest.cs
@@ -0,0 +1,38 @@
+﻿using BussinessLogicLayer.services;
+using DataAccessLayer.Interfaces;
+using FakeItEasy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace UnitTesting
+{
+    public class ReservationService_unitTest
+    {
+        readonly IReservationRepository reservationRepo;
+        readonly ReservationService reservationService;
+
+        readonly IBookRepository bookRepo;
+        readonly BookServices bookServices;
+
+        readonly IMembersRepository membersRepo;
+
+
+
+
+        public ReservationService_unitTest()
+        {
+            reservationRepo = A.Fake<IReservationRepository>();
+            bookRepo = A.Fake<IBookRepository>();
+            membersRepo = A.Fake<IMembersRepository>();
+            bookServices = new BookServices(bookRepo, reservationRepo, membersRepo);
+            reservationService = new ReservationService(reservationRepo, bookServices);
+        }
+
+        [Fact]
+        public void 
+    }
+}
