c97d57235146600af65e3f991fafffc29548dd15
vlovric21 <vlovric21@student.foi.hr>
2024-06-10 14:30:09 +0200

    Napisano za ArchiveServices

--- Software/E_Libra/UnitTesting/ArchiveServices_unitTest.cs
+++ Software/E_Libra/UnitTesting/ArchiveServices_unitTest.cs
@@ -1,5 +1,7 @@
-﻿using DataAccessLayer.Interfaces;
+﻿using BussinessLogicLayer.services;
+using DataAccessLayer.Interfaces;
 using DataAccessLayer.Repositories;
+using EntitiesLayer;
 using FakeItEasy;
 using System;
 using System.Collections.Generic;
@@ -16,10 +18,41 @@ namespace UnitTesting
         public void GetArchive_GivenFunctionIsCalled_ReturnsArchiveList()
         {
             //Arrange
+            IArchiveRepository repo = A.Fake<IArchiveRepository>();
+            IQueryable<ArchivedBookInfo> archivedBookInfos = new List<ArchivedBookInfo>
+            {
+                new ArchivedBookInfo { BookName = "Book1", EmployeeName = "Employee1", ArchiveDate = DateTime.Now },
+                new ArchivedBookInfo { BookName = "Book2", EmployeeName = "Employee2", ArchiveDate = DateTime.Now },
+            }.AsQueryable();
+            var service = new ArchiveServices(repo);
+
+            A.CallTo(() => repo.GetArchive()).Returns(archivedBookInfos);
 
             //Act
+            var act = service.GetArchive();
 
             //Assert
+            Assert.Equal(archivedBookInfos, act);
         }
+        [Fact]
+        public void GetArchivesForEmployee_GivenFunctionIsCalled_ReturnsArchiveList()
+        {
+            //Arrange
+            IArchiveRepository repo = A.Fake<IArchiveRepository>();
+            IQueryable<Archive> archives = new List<Archive>
+            {
+                new Archive { Book_id = 1, Employee_id = 1, arhive_date = DateTime.Now },
+                new Archive { Book_id = 2, Employee_id = 1, arhive_date = DateTime.Now },
+            }.AsQueryable();
+            var service = new ArchiveServices(repo);
+
+            A.CallTo(() => repo.GetArchivesForEmployee(1)).Returns(archives);
+
+            //Act
+            var act = service.GetArchivesForEmployee(1);
+
+            //Assert
+            Assert.Equal(archives, act);
+        }   
     }
 }
