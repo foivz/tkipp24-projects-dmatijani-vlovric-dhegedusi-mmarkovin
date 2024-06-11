92592360b0ef789e7f3ad8da744d77ea01c77491
vlovric21 <vlovric21@student.foi.hr>
2024-06-11 16:02:23 +0200

    Napravljen projekt za integracijsko testiranje

---
+++ Software/E_Libra/IntegrationTesting/ProbniIntTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace IntegrationTesting
+{
+    public class ProbniIntTest
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
