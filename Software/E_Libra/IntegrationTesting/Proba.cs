592f5a33f188581a926da9bbc65f29abfdca850a
vlovric21 <vlovric21@student.foi.hr>
2024-06-12 21:50:55 +0200

    Povezan projekt sa bazom

---
+++ Software/E_Libra/IntegrationTesting/Proba.cs
@@ -0,0 +1,30 @@
+﻿using BussinessLogicLayer.services;
+using EntitiesLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace IntegrationTesting
+{
+    public class Proba
+    {
+        [Fact]
+        public void Test1()
+        {
+            //Arrange
+            Member member = new Member
+            {
+                username = "anabol1",
+            };
+            MemberService memberService = new MemberService();
+            //Act
+            var result = memberService.CheckExistingUsername(member);
+
+            //Assert
+            Assert.True(result);
+        }
+    }
+}
