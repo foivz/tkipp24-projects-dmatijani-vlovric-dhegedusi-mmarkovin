906f6274a702e693ea3dd1b6847dd719bdf346a0
Magdalena Markovinović <mmarkovin21@foi.hr>
2024-06-13 21:07:57 +0200

    #42 Inicijaliziran test MemberService_integrationTest

---
+++ Software/E_Libra/IntegrationTesting/MemberService_integrationTest.cs
@@ -0,0 +1,22 @@
+﻿using BussinessLogicLayer.services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntegrationTesting
+{
+    public class MemberService_integrationTest
+    {
+        readonly DatabaseFixture fixture;
+        readonly MemberService memberService;
+
+        public MemberService_integrationTest(DatabaseFixture fixture)
+        {
+            memberService = new MemberService();
+            this.fixture = fixture;
+            this.fixture.ResetDatabase();
+        }
+    }
+}
