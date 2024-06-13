efe35f2f6adf2070c35da9e1c62c081fd7d23dd8
Domagoj Hegedušić <dhegedusi21@foi.hr>
2024-06-13 21:04:20 +0200

    Postavljena inicijalna klasa StatisticsService_integrationTest.

---
+++ Software/E_Libra/IntegrationTesting/StatisticsService_integrationTest.cs
@@ -0,0 +1,24 @@
+﻿using BussinessLogicLayer.services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace IntegrationTesting {
+
+    [Collection("Database collection")]
+    public class StatisticsService_integrationTest {
+
+        readonly StatisticsService statisticsService;
+        readonly DatabaseFixture fixture;
+
+        public StatisticsService_integrationTest(DatabaseFixture fixture)
+        {
+            statisticsService = new StatisticsService();
+            this.fixture = fixture;
+            this.fixture.ResetDatabase();
+        }
+    }
+}
