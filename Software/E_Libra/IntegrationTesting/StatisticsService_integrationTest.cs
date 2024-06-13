9079bac181d3f4a8783c4fb5dba34a8a4f8890e6
Domagoj Hegedušić <dhegedusi21@foi.hr>
2024-06-13 21:36:32 +0200

    Napravljen kod koji dodaje knjižnicu u bazu kao setup.

--- Software/E_Libra/IntegrationTesting/StatisticsService_integrationTest.cs
+++ Software/E_Libra/IntegrationTesting/StatisticsService_integrationTest.cs
@@ -1,4 +1,5 @@
 ﻿using BussinessLogicLayer.services;
+using EntitiesLayer;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,11 +15,29 @@ namespace IntegrationTesting {
         readonly StatisticsService statisticsService;
         readonly DatabaseFixture fixture;
 
+        readonly Library library;
+
         public StatisticsService_integrationTest(DatabaseFixture fixture)
         {
             statisticsService = new StatisticsService();
             this.fixture = fixture;
             this.fixture.ResetDatabase();
+
+            library = new Library {
+                id = 123,
+                name = "Testna knjiznica",
+                OIB = "11112222333",
+                price_day_late = 3.5m,
+                membership_duration = new DateTime(2024, 6, 23)
+            };
+            InsertLibraryIntoDatabase(library);
+
+        }
+
+        private void InsertLibraryIntoDatabase(Library library) {
+            var formattedMembershipDuration = library.membership_duration.ToString("yyyy-MM-dd");
+            string sqlInsertLibrary = $"INSERT [dbo].[Library] ([id], [name], [OIB], [price_day_late], [membership_duration]) VALUES ('{library.id}', '{library.name}', '{library.OIB}', {library.price_day_late}, '{formattedMembershipDuration}');";
+            Helper.ExecuteCustomSql(sqlInsertLibrary);
         }
     }
 }
