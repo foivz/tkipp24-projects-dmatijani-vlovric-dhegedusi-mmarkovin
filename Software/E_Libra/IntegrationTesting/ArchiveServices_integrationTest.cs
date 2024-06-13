71e0a9a00b935812084761d1b5bf773f73994ab2
vlovric21 <vlovric21@student.foi.hr>
2024-06-13 16:36:29 +0200

    Napravljen ArchiveService

---
+++ Software/E_Libra/IntegrationTesting/ArchiveServices_integrationTest.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using FluentAssertions;
+using DataAccessLayer.Repositories;
+using BussinessLogicLayer.services;
+
+namespace IntegrationTesting
+{
+    public class ArchiveServices_integrationTest
+    {
+        ArchiveServices service;
+
+        string createLibrary =
+            "INSERT [dbo].[Library] ([id], [name], [OIB], [phone], [email], [price_day_late], [address], [membership_duration]) " +
+            "VALUES (1, N'Knjiznica', 12345, 331, N'email', 3, N'adresa', GETDATE())";
+
+        string createBooks =
+            "INSERT [dbo].[Book] ([name], [pages_num], [digital], [barcode_id], [total_copies], [current_copies], [Genre_id],  [Library_id]) " +
+            "VALUES (N'Book1', 10, 0, 12345, 10, 10, 1, 1); " +
+            "INSERT [dbo].[Book] ([name], [pages_num], [digital], [barcode_id], [total_copies], [current_copies], [Genre_id],  [Library_id]) " +
+            "VALUES (N'Book2', 10, 0, 12346, 10, 10, 1, 1); ";
+       
+        string createEmployees =
+             "INSERT [dbo].[Employee] ([name], [surname], [username], [password], [OIB], [Library_id]) " +
+            "VALUES (N'ime1', N'prezime1', N'Employee1', N'123', N'1234', 1) " +
+             "INSERT [dbo].[Employee] ([name], [surname], [username], [password], [OIB], [Library_id]) " +
+            "VALUES (N'ime2', N'prezime2', N'Employee2', N'123', N'1235', 1) ";
+
+        string createGenres =
+            "INSERT [dbo].[Genre] ([name]) VALUES (N'zanr')";
+        
+        string createArchives =
+            "INSERT [dbo].[Archive] ([Book_id], [Employee_id], [arhive_date]) " +
+            "VALUES (1, 1, GETDATE()); " +
+            "INSERT [dbo].[Archive] ([Book_id], [Employee_id], [arhive_date]) " +
+            "VALUES (2, 2, GETDATE()); ";
+
+        List<ArchivedBookInfo> archivedBookInfos = new List<ArchivedBookInfo>
+            {
+                new ArchivedBookInfo { BookName = "Book1", EmployeeName = "ime1 prezime1", ArchiveDate = DateTime.Now.Date },
+                new ArchivedBookInfo { BookName = "Book2", EmployeeName = "ime2 prezime2", ArchiveDate = DateTime.Now.Date },
+            };
+
+        public ArchiveServices_integrationTest()
+        {
+            Helper.ResetDatabase();
+            service = new ArchiveServices();
+        }
+
+        [Fact]
+        public void GetArchive_GivenFunctionIsCalled_ReturnsArchiveList()
+        {
+            //Arrange
+            Helper.ExecuteCustomSql(createLibrary);
+            Helper.ExecuteCustomSql(createGenres);
+            Helper.ExecuteCustomSql(createBooks);
+            Helper.ExecuteCustomSql(createEmployees);
+            Helper.ExecuteCustomSql(createArchives);
+
+            //Act
+            var result = service.GetArchive();
+
+
+            //Assert
+            result.Should().BeEquivalentTo(archivedBookInfos);
+        }
+
+        [Fact]
+        public void GetArchivesForEmployee_GivenFunctionIsCalled_ReturnsArchiveList()
+        {
+            //Arrange
+            Helper.ExecuteCustomSql(createLibrary);
+            Helper.ExecuteCustomSql(createGenres);
+            Helper.ExecuteCustomSql(createBooks);
+            Helper.ExecuteCustomSql(createEmployees);
+            Helper.ExecuteCustomSql(createArchives);
+
+            //Act
+            var result = service.GetArchivesForEmployee(1);
+
+            //Assert
+            result.Should().HaveCount(1);
+        }
+    }
+}
