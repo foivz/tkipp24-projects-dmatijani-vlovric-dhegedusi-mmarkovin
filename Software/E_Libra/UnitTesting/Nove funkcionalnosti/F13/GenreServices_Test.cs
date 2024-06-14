16d4f34c00cdbfb7c2ba63d1383f91d0d2abed70
vlovric21 <vlovric21@student.foi.hr>
2024-06-14 19:51:45 +0200

    Napravljen test za žanr

--- Software/E_Libra/UnitTesting/Nove funkcionalnosti/F13/GenreServices_Test.cs
+++ Software/E_Libra/UnitTesting/Nove funkcionalnosti/F13/GenreServices_Test.cs
@@ -13,6 +13,33 @@ namespace UnitTesting.Nove_funkcionalnosti.F13
 {
     public class GenreServices_Test
     {
-        
+        readonly IGenreRepository repo;
+        readonly GenreServices service;
+
+        public GenreServices_Test()
+        {
+            repo = A.Fake<IGenreRepository>();
+            service = new GenreServices(repo);
+        }
+
+        //Viktor Lovrić
+        [Fact]
+        public void SearchGenres_GivenStringIsPassed_ReturnsGenres()
+        {
+            //Arrange
+            var genres = new List<Genre>
+            {
+                new Genre { name = "Genre1" },
+                new Genre { name = "Genre2" }
+            };
+
+            A.CallTo(() => repo.SearchGenre("Genre")).Returns(genres);
+
+            //Act
+            var result = service.SearchGenres("Genre");
+
+            //Assert
+            Assert.Equal(genres, result);
+        }
     }
 }
