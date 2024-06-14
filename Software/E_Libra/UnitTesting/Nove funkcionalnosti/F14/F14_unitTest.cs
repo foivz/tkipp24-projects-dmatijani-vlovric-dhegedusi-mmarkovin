8041c84bed30848565f106727cf8a3984aa1eac6
Magdalena Markovinović <mmarkovin21@foi.hr>
2024-06-14 21:42:42 +0200

    #44 Napravljena početna klasa testa za f14

---
+++ Software/E_Libra/UnitTesting/Nove funkcionalnosti/F14/F14_unitTest.cs
@@ -0,0 +1,28 @@
+﻿using BussinessLogicLayer.services;
+using DataAccessLayer.Interfaces;
+using EntitiesLayer;
+using FakeItEasy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTesting.Nove_funkcionalnosti.F14
+{
+    public class F14_unitTest
+    {
+        private MemberService memberService;
+        private IMembersRepository membersRepository;
+
+        public F14_unitTest()
+        {
+            membersRepository = A.Fake<IMembersRepository>();
+            memberService = new MemberService(membersRepository, null, null, null, null);
+
+            LoggedUser.Username = null;
+            LoggedUser.UserType = null;
+            LoggedUser.LibraryId = 0;
+        }
+    }
+}
