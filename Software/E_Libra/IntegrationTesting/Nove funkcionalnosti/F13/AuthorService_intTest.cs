b851c2640811b3ec56e454156bd6f178b021bda8
vlovric21 <vlovric21@student.foi.hr>
2024-06-14 20:07:40 +0200

    Postavljene testne klase za integracijsko testiranje

---
+++ Software/E_Libra/IntegrationTesting/Nove funkcionalnosti/F13/AuthorService_intTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using FluentAssertions;
+using BussinessLogicLayer.services;
+
+namespace IntegrationTesting.Nove_funkcionalnosti.F13
+{
+    [Collection("Database collection")]
+    public class AuthorService_intTest
+    {
+        readonly AuthorService service;
+        readonly DatabaseFixture fixture;
+
+        public AuthorService_intTest(DatabaseFixture fixture)
+        {
+            this.fixture = fixture;
+            this.fixture.ResetDatabase();
+            service = new AuthorService();
+        }
+
+    }
+}
---
+++ Software/E_Libra/IntegrationTesting/Nove funkcionalnosti/F13/GenreServices_intTest.cs
@@ -0,0 +1,23 @@
+﻿using BussinessLogicLayer.services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntegrationTesting.Nove_funkcionalnosti.F13
+{
+    public class GenreServices_intTest
+    {
+        readonly GenreServices services;
+        readonly DatabaseFixture fixture;
+
+        public GenreServices_intTest(DatabaseFixture fixture)
+        {
+            this.fixture = fixture;
+            this.fixture.ResetDatabase();
+            services = new GenreServices();
+        }
+
+    }
+}
