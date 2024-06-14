348c198d9431a7be5bf5c093e83dc6e61d6fb30b
vlovric21 <vlovric21@student.foi.hr>
2024-06-14 19:49:21 +0200

    Postavljene testne klase i napravljen test za autora

---
+++ Software/E_Libra/UnitTesting/Nove funkcionalnosti/F13/GenreServices_Test.cs
@@ -0,0 +1,18 @@
+﻿using BussinessLogicLayer.services;
+using DataAccessLayer.Interfaces;
+using EntitiesLayer;
+using FakeItEasy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace UnitTesting.Nove_funkcionalnosti.F13
+{
+    public class GenreServices_Test
+    {
+        
+    }
+}
