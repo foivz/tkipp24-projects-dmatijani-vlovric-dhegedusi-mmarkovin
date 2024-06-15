1e9cd91c5d93f1d03a60a4379d9279e3e278b8d9
David Matijanić <dmatijani21@foi.hr>
2024-06-15 12:18:05 +0200

    #46 CRVENA: Napisan test koji se ne može kompilirati
    Ne može se kompilirati jer niti nema klase GptService.

---
+++ Software/E_Libra/UnitTesting/Nove funkcionalnosti/F16/GPTService_integrationTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace UnitTesting.Nove_funkcionalnosti.F16
+{
+    //David Matijanić
+    public class GPTService_integrationTest
+    {
+        [Fact]
+        public void GPTService_SetEmptySystemMessage_Runs()
+        {
+            //Arrange
+            var gptService = new GPTService();
+
+            //Act
+            var result = gptService.SetSystemMessage("");
+
+            //Assert
+            result.Should().Be(true);
+        }
+    }
+}
