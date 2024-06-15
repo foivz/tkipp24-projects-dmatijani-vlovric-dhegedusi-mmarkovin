f4c5189936f7415903dfdccb69884616c3f1866a
Domagoj Hegedušić <dhegedusi21@foi.hr>
2024-06-15 18:38:24 +0200

    Napravljeni Unit i Integracijski testovi.

---
+++ Software/E_Libra/IntegrationTesting/Nove funkcionalnosti/F15/BookService_integrationTest.cs
@@ -0,0 +1,56 @@
+﻿using BussinessLogicLayer.services;
+using EntitiesLayer;
+using EntitiesLayer.Entities;
+using System;
+using System.Collections.Generic;
+using Xunit;
+using FluentAssertions;
+
+namespace IntegrationTesting.Nove_funkcionalnosti.F15 {
+    [Collection("Database collection")]
+    public class BookService_integrationTest {
+        readonly BookServices _bookService;
+        readonly DatabaseFixture fixture;
+
+        public BookService_integrationTest(DatabaseFixture fixture) {
+            _bookService = new BookServices();
+            this.fixture = fixture;
+            this.fixture.ResetDatabase();
+        }
+
+        [Fact]
+        public void GetTopBorrowedBooks_ReturnsCorrectBooks() {
+            // Arrange
+            string sql =
+                "INSERT [dbo].[Library] ([id], [name], [OIB], [phone], [email], [price_day_late], [address], [membership_duration]) VALUES (1, N'Knjiznica', 123, 331, N'email', 3, N'adresa', GETDATE());" +
+                "INSERT [dbo].[Member] ([name], [surname], [username], [password], [OIB], [membership_date], [barcode_id], [Library_id]) VALUES (N'Ivo', N'Ivic', N'iivic', N'ivo123', '57647557445', GETDATE(), N'B001', 1);" +
+                "INSERT [dbo].[Member] ([name], [surname], [username], [password], [OIB], [membership_date], [barcode_id], [Library_id]) VALUES (N'Ana', N'Anic', N'aanic', N'ana123', '64363434343', GETDATE(), N'B002', 1);" +
+                "INSERT [dbo].[Genre] ([name]) VALUES (N'Tragedija');" +
+                "INSERT [dbo].[Genre] ([name]) VALUES (N'Drama');" +
+                "INSERT [dbo].[Author] ([idAuthor], [name], [surname], [birth_date]) VALUES (1, N'William', N'Shakespare', GETDATE());" +
+                "INSERT [dbo].[Author] ([idAuthor], [name], [surname], [birth_date]) VALUES (2, N'Homer', N'Borovski', GETDATE());" +
+                "INSERT [dbo].[Book] ([name], [description], [publish_date], [pages_num], [digital], [url_photo], [barcode_id], [total_copies], [current_copies], [Genre_id], [Library_id]) VALUES (N'Hamlet', N'Nema opisa', GETDATE(), 300, 1, N'slika1', N'BC0011', 5, 3, 1, 1);" +
+                "INSERT [dbo].[Book] ([name], [description], [publish_date], [pages_num], [digital], [url_photo], [barcode_id], [total_copies], [current_copies], [Genre_id], [Library_id]) VALUES (N'Ilijada', N'Description 2', GETDATE(), 400, 0, N'slika2', N'BC0022', 10, 7, 2, 1);" +
+                "INSERT [dbo].[Employee] ([name], [surname], [username], [password], [OIB], [Library_id]) VALUES (N'Darko', N'Daric', N'ddaric', N'jakalozinka', '11892593283', 1);" +
+                "INSERT [dbo].[Employee] ([name], [surname], [username], [password], [OIB], [Library_id]) VALUES (N'Marina', N'Misic', N'mmisic', N'mypw', '85738923405', 1);" +
+                "INSERT INTO [dbo].[Book_Author] ([Book_id], [Author_idAuthor]) VALUES (1, 1);" +
+                "INSERT INTO [dbo].[Book_Author] ([Book_id], [Author_idAuthor]) VALUES (2, 2);" +
+                "INSERT [dbo].[Borrow] ([Book_id], [Member_id], [borrow_status], [borrow_date], [return_date], [Employee_borrow_id], [Employee_return_id]) VALUES (1, 1, 0, GETDATE(), GETDATE(), 1, 1);" +
+                "INSERT [dbo].[Borrow] ([Book_id], [Member_id], [borrow_status], [borrow_date], [return_date], [Employee_borrow_id], [Employee_return_id]) VALUES (2, 1, 0, GETDATE(), GETDATE(), 1, 1);" +
+                "INSERT [dbo].[Borrow] ([Book_id], [Member_id], [borrow_status], [borrow_date], [return_date], [Employee_borrow_id], [Employee_return_id]) VALUES (1, 2, 0, GETDATE(), GETDATE(), 1, 1);";
+            Helper.ExecuteCustomSql(sql);
+
+            var expectedBooks = new List<MostPopularBooks>
+            {
+                new MostPopularBooks { Book_Name = "Hamlet", Author_Name = "William Shakespare", Times_Borrowed = 2, Url_Photo = "slika1", Order_Number = 1 },
+                new MostPopularBooks { Book_Name = "Ilijada", Author_Name = "Homer Borovski", Times_Borrowed = 1, Url_Photo = "slika2", Order_Number = 2 }
+            };
+
+            // Act
+            var result = _bookService.GetTopBorrowedBooks(1);
+
+            // Assert
+            result.Should().BeEquivalentTo(expectedBooks);
+        }
+    }
+}
---
+++ Software/E_Libra/UnitTesting/Nove funkcionalnosti/F15/BookService_unitTest.cs
@@ -0,0 +1,50 @@
+﻿using FakeItEasy;
+using DataAccessLayer.Interfaces;
+using BussinessLogicLayer.services;
+using EntitiesLayer.Entities;
+using System.Collections.Generic;
+using Xunit;
+
+namespace UnitTesting.Nove_funkcionalnosti.F15 {
+    public class BookService_unitTest {
+        readonly IBookRepository _bookRepo;
+        readonly IReservationRepository _reservationRepo;
+        readonly IMembersRepository _memberRepo;
+        readonly BookServices _bookService;
+
+        public BookService_unitTest() {
+            _bookRepo = A.Fake<IBookRepository>();
+            _reservationRepo = A.Fake<IReservationRepository>();
+            _memberRepo = A.Fake<IMembersRepository>();
+            _bookService = new BookServices(_bookRepo, _reservationRepo, _memberRepo);
+        }
+
+        // Domagoj Hegedušić
+        [Fact]
+        public void GetTopBorrowedBooks_GivenLibraryId_ReturnsTopBorrowedBooks() {
+            // Arrange
+            int libraryId = 1;
+            var expectedBooks = new List<MostPopularBooks>
+            {
+                new MostPopularBooks { Book_Name = "Hamlet", Author_Name = "William Shakespeare", Times_Borrowed = 10, Url_Photo = "probniurl.com", Order_Number = 1 },
+                new MostPopularBooks { Book_Name = "Odiseja", Author_Name = "Homer", Times_Borrowed = 8, Url_Photo = "probniurl.com", Order_Number = 2 }
+            };
+
+            A.CallTo(() => _bookRepo.GetTopBooks(libraryId)).Returns(expectedBooks);
+
+            // Act
+            var result = _bookService.GetTopBorrowedBooks(libraryId);
+
+            // Assert
+            Assert.Equal(expectedBooks.Count, result.Count);
+            for (int i = 0; i < expectedBooks.Count; i++) {
+                Assert.Equal(expectedBooks[i].Book_Name, result[i].Book_Name);
+                Assert.Equal(expectedBooks[i].Author_Name, result[i].Author_Name);
+                Assert.Equal(expectedBooks[i].Times_Borrowed, result[i].Times_Borrowed);
+                Assert.Equal(expectedBooks[i].Url_Photo, result[i].Url_Photo);
+                Assert.Equal(expectedBooks[i].Order_Number, result[i].Order_Number);
+            }
+        }
+
+    }
+}
