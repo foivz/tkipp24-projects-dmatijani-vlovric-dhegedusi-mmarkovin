ee16a2dd734a21d4a86d10f77a0392f9d58caed9
David Matijanić <dmatijani21@foi.hr>
2024-06-15 20:13:12 +0200

    #46 Do kraja napravljeno sučelje za LibrAI

--- Software/E_Libra/PresentationLayer/LibrAI_Panel.xaml.cs
+++ Software/E_Libra/PresentationLayer/LibrAI_Panel.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BussinessLogicLayer.F16;
+using DataAccessLayer.F16;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,9 +21,109 @@ namespace PresentationLayer
     /// </summary>
     public partial class LibrAI_Panel : Window
     {
+        private bool EnteredApiKey = false;
+        private IGPTRequestSender gptRequestSender { get; set; }
+        private GPTService gptService { get; set; }
+        private bool WaitingForResponse = false;
+
         public LibrAI_Panel()
         {
             InitializeComponent();
+        }
+
+        private void btnSaveApiKey_Click(object sender, RoutedEventArgs e)
+        {
+            if (EnteredApiKey)
+            {
+                return;
+            }
+
+            string apiKey = GetEnteredApiKey();
+            if (apiKey == "")
+            {
+                ChangeLaptopImage("angry");
+                MessageBox.Show("Nije valjan API ključ!");
+                ChangeLaptopImage("introduction");
+                return;
+            }
+
+            tbResponse.Text = "";
+            EnteredApiKey = true;
+            ChangeLaptopImage("responding");
+            btnSendRequest.IsEnabled = true;
+            tbEnterApiKey.Visibility = Visibility.Collapsed;
+            spApiKeyEnter.Visibility = Visibility.Collapsed;
+            spQuestionAnswer.Visibility = Visibility.Visible;
+
+            CreateGPTService(apiKey);
+        }
+
+        private async void btnSendRequest_Click(object sender, RoutedEventArgs e)
+        {
+            if (WaitingForResponse)
+            {
+                return;
+            }
+
+            var textToSend = txtRequest.Text;
+            if (String.IsNullOrWhiteSpace(textToSend))
+            {
+                tbResponse.Text = "Unesite valjano pitanje!";
+                return;
+            }
+
+            WaitingForResponse = true;
+            ChangeLaptopImage("thinking");
+            await SendAPIRequest(txtRequest.Text);
+        }
+
+        private async Task SendAPIRequest(string request)
+        {
+            var response = await gptService.SendUserMessage(request);
+            tbResponse.Text = response;
+            WaitingForResponse = false;
+            ChangeLaptopImage("responding");
+        }
+
+        private string GetEnteredApiKey()
+        {
+            if (EnteredApiKey)
+            {
+                return "";
+            }
+
+            if (String.IsNullOrWhiteSpace(pbApiKey.Password))
+            {
+                return "";
+            }
+            return pbApiKey.Password.Trim();
+        }
+
+        private void ChangeLaptopImage(string image)
+        {
+            imgLaptop.Source = new BitmapImage(new Uri($@"/Images/LibrAI_Images/{image}.png", UriKind.Relative));
+        }
+
+        private void CreateGPTService(string apiKey)
+        {
+            gptRequestSender = new GPTRequestSender(apiKey);
+            gptService = new GPTService(gptRequestSender);
+        }
+
+        private void txtRequest_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (!WaitingForResponse && EnteredApiKey)
+            {
+                ChangeLaptopImage("listening");
+            }
+        }
+
+        private void txtRequest_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (!WaitingForResponse && EnteredApiKey)
+            {
+                ChangeLaptopImage("responding");
+            }
         }
     }
 }
